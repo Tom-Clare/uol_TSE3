db1c2751415dfb024d57402d5d6390bacb61405e
Tom Clare <tclare5@gmail.com>
2021-05-12 18:09:05 +0100

    cleaning sweep of minor details

--- server/algo.py
+++ server/algo.py
@@ -6,47 +6,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-article_true = """
-Arlene Foster will quit the Democratic Unionist Party (DUP) when she stands down as Northern Ireland's first minister, BBC News NI understands.
-
-Sources close to her said she thinks it is no longer the party she joined and it is moving in a different direction.
-
-Mrs Foster is to resign as DUP leader on 28 May and end her tenure as first minister at the end of June.
-
-On Thursday, Agriculture Minister Edwin Poots declared his intention to stand for the party leadership.
-
-Speaking on Friday, Mrs Foster said she would wait until she stepped down as first minister before making public her decision on whether she will remain in the DUP.
-
-On a visit to a primary school in Cloughey in County Down she added that none of her colleagues who signed a letter of no confidence in her had spoken to her since.
-
-Mrs Foster has led the DUP since December 2015.
-
-The following month she was appointed first minister, becoming the first woman and the youngest person to hold both jobs.
-In her resignation statement on Wednesday, Mrs Foster said she was preparing to leave the political stage.
-
-BBC News NI understands that means not only quitting as a Stormont assembly member but severing her ties with the party she has led since December 2015.
-
-On Thursday she informed her constituency association in Fermanagh and South Tyrone of her decision.
-
-Quizzed on Friday whether she would go to the House of Lords, she said that was a matter for others.
-Asked about her party's future, she wished her successor and the party well.
-
-She also said not all her colleagues had signed the motion of no confidence and said some "good friends" did not.
-Mrs Foster announced her resignation after about 80% of the DUP's Stormont and Westminster ranks signed a letter of no confidence in her leadership.
-She has endured several major challenges at the helm of the party, including division among members on social issues and significant recent pressure resulting from the fallout from Brexit, which the party supported.
-He has secured the backing of senior assembly member Christopher Stalford, who said his friend had the "experience and talents to take our country and our party forward".
-Securing the support of a majority of the party's 27 members of the legislative assembly (MLAs) will be crucial to his hopes of replacing Mrs Foster.
-Only a small number of the DUP's membership - its MLAs and MPs - will get to vote in a leadership contest.
-"""
-
-article_false = """
-The Pacific Northwest tree octopus (Octopus paxarbolis) can be found in the temperate rainforests of the Olympic Peninsula on the west coast of North America. Their habitat lies on the Eastern side of the Olympic mountain range, adjacent to Hood Canal. These solitary cephalopods reach an average size (measured from arm-tip to mantle-tip,) of 30-33 cm. Unlike most other cephalopods, tree octopuses are amphibious, spending only their early life and the period of their mating season in their ancestral aquatic environment. Because of the moistness of the rainforests and specialized skin adaptations, they are able to keep from becoming desiccated for prolonged periods of time, but given the chance they would prefer resting in pooled water.
-
-An intelligent and inquisitive being (it has the largest brain-to-body ratio for any mollusk), the tree octopus explores its arboreal world by both touch and sight. Adaptations its ancestors originally evolved in the three dimensional environment of the sea have been put to good use in the spatially complex maze of the coniferous Olympic rainforests. The challenges and richness of this environment (and the intimate way in which it interacts with it,) may account for the tree octopus's advanced behavioral development. (Some evolutionary theorists suppose that "arboreal adaptation" is what laid the groundwork in primates for the evolution of the human mind.)
-
-Reaching out with one of her eight arms, each covered in sensitive suckers, a tree octopus might grab a branch to pull herself along in a form of locomotion called tentaculation; or she might be preparing to strike at an insect or small vertebrate, such as a frog or rodent, or steal an egg from a bird's nest; or she might even be examining some object that caught her fancy, instinctively desiring to manipulate it with her dexterous limbs (really deserving the title "sensory organs" more than mere "limbs",) in order to better know it.
-"""
-
 #Converts string into an array
 #Will return None type if data is not a string
 def convertToArray(data):
--- server/api-v1.py
+++ server/api-v1.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, Response, jsonify
+from flask import Flask, redirect, url_for, render_template, request, Response
 import algo as algo
 
 app = Flask(__name__)
@@ -8,19 +8,11 @@ def login():
     response = Response()
     response.headers["Access-Control-Allow-Origin"] = "*"
     
-
-    #result = "pulls the result from the algorithm"
-    #result could be formatted as a list
-    #return "returns desired string format for the json"
     result = algo.scan(algo.convertToArray(request.form["content"]))
-
     responsedata = '"verdict": "' + str(result[0]) + '", "confidence": "' + str(round(algo.getScore(), 2)) + '"'
+
     response.set_data('{' + responsedata + '}')
     return response
-
-def find_results():
-    #do something
-    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
